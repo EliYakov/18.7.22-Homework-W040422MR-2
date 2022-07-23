5c8cf79d4949d6ec6ee7f0000a2c256190fac95b
EliYakov <eliko0807@gmail.com>
2022-07-23 23:00:58 +0300

    JS Done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const nameInput = document.getElementById("name-input");
 const numberInput = document.getElementById("number-input");
+const femaleInput = document.getElementById("female-input");
+const maleInput = document.getElementById("male-input");
 const Card = document.getElementById("card");
 const colorInput = document.getElementById("color-input");
 const countryInput = document.getElementById("country-input");
@@ -15,12 +17,23 @@ function resetInputs() {
 }
 
 function onBtnClick() {
+  setGender();
   checkAge();
   setUserFavoriteColor(colorInput.value);
 
+  let gender = "";
+  if (femaleInput.checked) {
+    gender = femaleInput.value;
+  }
+  if (maleInput.checked) {
+    gender = maleInput.value;
+  }
+
   Card.innerHTML +=
     `<div class="card-body">
-    <h5 class="card-title">Card Profile</h5>
+    <h5 class="card-title">` +
+    setGender() +
+    `</h5>
     <p class="card-text">
       Hello !! My name is ` +
     nameInput.value +
@@ -39,5 +52,15 @@ function checkAge() {
   }
   if (numberInput.value <= 0) {
     Card = "";
+  }
+}
+
+function setGender() {
+  if (femaleInput.checked) {
+    return "female";
+  }
+
+  if (maleInput.checked) {
+    return "male";
   }
 }
